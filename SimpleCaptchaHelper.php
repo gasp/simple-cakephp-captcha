bc5666920bc1eff423fd3f2f6f3b54e764b428c1
gaspard <gaspard@gmail.com>
2012-10-10 12:09:38 +0200

    initial commit

---
+++ SimpleCaptchaHelper.php
@@ -0,0 +1,31 @@
+<?php  
+/** 
+ * SimpleCaptchaHelper
+ * 
+ * @version 1.0 
+ * @author gaspard from freelancis.com
+ * @license MIT Style License 
+ */ 
+App::uses('Helper', 'View');
+class SimpleCaptchaHelper extends AppHelper { 
+
+	public function __construct($config) {
+
+	}
+
+
+
+
+	/**
+	 * stores the image
+	 *
+	 * @param string $value 
+	 * @return string $imageurl
+	 * @author gaspard
+	 */
+	public function store($value='') {
+		# code...
+	}
+	
+	
+}
